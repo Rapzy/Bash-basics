15eea8d6388ee855ad9a48e5894617b9004ea7b2
Egor Andrushko <andrushko.egor@gmail.com>
2018-05-06 23:08:51 +0300

    Delete validation

--- lab1.sh
+++ lab1.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ #!/bin/bash
 
 PROGNAME=$(basename $0)
 
@@ -6,38 +6,10 @@ MIN_FSIZE=$1
 MAX_FSIZE=$2
 FOUT=$3
 
-function is_positive_integer
-{
-    local num=$1
-    return $([[ $num =~ ^(0|[1-9][0-9]*)$ ]])
-}
-
-if [ $# -lt 3 ]
-    then
-        echo "$PROGNAME: missing operand"
-        exit 1
-    elif ! is_positive_integer $MIN_FSIZE
-    then
-        echo "$PROGNAME: first argument must be a positive integer"
-        exit 1
-    elif ! is_positive_integer $MAX_FSIZE
-    then
-        echo "$PROGNAME: second argument must be a positive integer"
-        exit 1
-    elif [ $MAX_FSIZE -lt $MIN_FSIZE ]
-    then
-        echo "$PROGNAME: min size can not be greater than max size"
-        exit 1
-    fi
-
-truncate -s 0 $FOUT 
-files=$(find . -type f)
+files=$(find . -type f -size +$MIN_FSIZE -and -size -$MAX_FSIZE -or -size $MIN_FSIZE -or -size $MAX_FSIZE 2> errors.txt)
 for file in $files
 do
-  fsize=$(wc -c < $file)
-  if [[ $fsize -ge $MIN_FSIZE &&  $fsize -le $MAX_FSIZE ]]; then
-  	echo $(realpath $file) $(basename $file) $fsize 'bytes' >> $FOUT 
-	fi
+  echo $(realpath $file) $(basename $file) $(wc -c < $file) 'bytes' 2>> errors.txt >> $FOUT
 done
-echo "$PROGNAME: viewed $(wc -w <<< $files) files"
+echo "$PROGNAME: viewed $(wc -l <<< $files) files"
 exit 0
