385923b05399d6ee80f6cf39edb7966653182fa8
Egor Andrushko <andrushko.egor@gmail.com>
2018-05-15 21:44:21 +0300

    Refactor code

--- lab1.sh
+++ lab1.sh
@@ -6,10 +6,12 @@ MIN_FSIZE=$1
 MAX_FSIZE=$2
 FOUT=$3
 
-files=$(find . -type f -size +$MIN_FSIZE -and -size -$MAX_FSIZE -or -size $MIN_FSIZE -or -size $MAX_FSIZE 2> errors.txt)
-for file in $files
+find "$PWD/" -type f \( -size +$MIN_FSIZE -and -size -$MAX_FSIZE -or -size $MIN_FSIZE -or -size $MAX_FSIZE \) -printf "%h/%f %f %s bytes\n" > $FOUT 2> errors.txt
+echo "$PROGNAME: viewed $(wc -l < $FOUT) files"
+
+exec 2>&1 
+while read line
 do
-  echo $(realpath $file) $(basename $file) $(wc -c < $file) 'bytes' 2>> errors.txt >> $FOUT
-done
-echo "$PROGNAME: viewed $(wc -l <<< $files) files"
+  echo "$PROGNAME: $line" 1>&2
+done <errors.txt
 exit 0
